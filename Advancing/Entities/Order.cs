e393ca6cf57c826eee5408fc0efcaaeca12138ff
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2023-02-10 15:29:21 -0300

    Iniciando o nível intermediário do curso do Nélio Alves

---
+++ Advancing/Entities/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Advancing.Entities.Enums;
+
+namespace Advancing.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status{ get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+                
+
+
+        }
+    }
+}
---
+++ Advancing/Program.cs
@@ -0,0 +1,23 @@
+﻿using Advancing.Entities.Enums;
+using Advancing.Entities;
+
+
+namespace Advancing
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.UtcNow,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+        }
+    }
+}
